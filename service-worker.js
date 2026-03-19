5029e54d67dc2576885e3c7bd495fe3ca2a8626a
Dev-RicardoAlmeida <148581496+Dev-RicardoAlmeida@users.noreply.github.com>
2026-03-19 12:27:36 -0400

    Upload do Service Worker

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = "quitacao-cache-v1";
+const FILES = [
+    "index.html",
+    "manifest.webmanifest",
+    "icon.png"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
+});
